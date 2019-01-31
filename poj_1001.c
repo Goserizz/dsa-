36720a858e394b2a7efb2435b3dabfa06f9dcbbc
goserizz <goserizz@gmail.com>
2019-01-31 21:31:16 +0800

    /* empty */

---
+++ poj_1001.c
@@ -0,0 +1,69 @@
+//
+//  poj_1001.c
+//  Data Structure & Algorithm
+//
+//  Created by lrf on 2019/1/29.
+//  Copyright © 2019 lrf. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\n");
+    char r[7];
+    int n;
+    int *exp, *temp = NULL, *result;
+    int i, j = 0, k, dot, mul;
+    exp = (int*) malloc (5 * sizeof(int));
+    result = (int*) malloc (5 * sizeof(int));
+    
+    while (scanf ("%s%d", r, &n) == 2){
+        for (i = 5, j = 0; i > -1; i --)
+            if (r[i] != '.')
+                exp[j ++] = r[i] - 48;
+            else
+                dot = 5 - i;
+        
+        memcpy (result, exp, 5 * sizeof (int));
+        
+        for (k = 2; k < n + 1; k ++){
+            temp = (int*) malloc (5 * k * sizeof (int));
+            memset (temp, 0, 5 * k * sizeof (int));
+            for (i = 0; i < 5; i ++){
+                j = 0;
+                for (j = 0; j < 5 * k - 5; j ++){
+                    mul = exp[i] * result[j];
+                    temp[j + i] += mul % 10;
+                    temp[j + i + 1] += mul / 10;
+                    // printf ("%d: %d\n", j + i, temp[j + i]);
+                }
+            }
+            free (result);
+            result = temp;
+        }
+        
+        for (i = 0; i < 5 * n; i ++){
+            result[i + 1]  += result[i] / 10;
+            result[i] %= 10;
+        }
+        
+        i = 5 * n - 1;
+        while (result[i] == 0 && i > dot * n - 1)
+            i --;
+        j = 0;
+        while (result[j] == 0)
+            j ++;
+        for (; i > dot * n - 1; i --)
+            printf ("%d", result[i]);
+        if (j <= i){
+            printf (".");
+            for (; i >= j; i --)
+                printf ("%d", result[i]);
+        }
+        printf ("\n");
+    }
+    return 0;
+}
---
+++ poj_1753.c
@@ -0,0 +1,83 @@
+//
+//  poj_1753.c
+//  Data Structure & Algorithm
+//
+//  Created by lrf on 2019/1/31.
+//  Copyright © 2019 lrf. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned short change[16] = {
+        19, 39, 78, 140,
+        305, 626, 1252, 2248,
+        4880, 10016, 20032, 35968,
+        12544, 29184, 58368, 51200
+};
+
+struct flip{
+    char* try;
+    short chess;
+};
+
+int main(){
+    int prev = 16, now = 120, j, top;
+    char c, i, k;
+    short chess = 0;
+    struct flip *flips = (struct flip *) malloc (16 * sizeof (struct flip));
+    struct flip *temp;
+
+    for (i = 0; i < 16; i ++) {
+        while (scanf("%c", &c))
+            if (c != 10)
+                break;
+        if (c == 'b')
+            chess |= 1 << i;
+    }
+
+    if (chess == 0 || chess == -1){
+        printf("0\n");
+        return 0;
+    }
+
+    for (i = 0; i < 16; i ++) {
+        flips[i].try = (char*) malloc (sizeof(char));
+        flips[i].try[0] = i;
+        flips[i].chess = chess ^ change[i];
+        if (flips[i].chess == 0 || flips[i].chess == -1){
+            printf("1\n");
+            return 0;
+        }
+    }
+
+    for (i = 1; i < 6; i ++) {
+        top = 0;
+        temp = (struct flip*) malloc (now * sizeof (struct flip));
+        for (j = 0; j < prev; j++) {
+            for (k = flips[j].try[i - 1] + 1; k < 16; k ++) {
+                temp[top].try = (char*) malloc ((i + 1) * sizeof (char));
+                memcpy (temp[top].try, flips[j].try, i);
+                temp[top].try[i] = k;
+                temp[top].chess = flips[j].chess ^ change[k];
+                if (temp[top].chess == 0 || temp[top].chess == -1){
+                    printf ("%d\n", i + 1);
+                    return 0;
+                }
+                top ++;
+            }
+        }
+
+        prev = now;
+        now = 1;
+        for (j = 1; j < i + 3; j ++)
+            now = now * (17 - j) / j;
+
+        free (flips);
+        flips = temp;
+    }
+
+    printf ("Impossible\n");
+    return 0;
+}
